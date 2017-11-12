a2e05c457b0dc34d1c944f6ccc61583702fad50e
changlezhong <63912620@qq.com>
2017-11-12 10:17:45 +0800

    修改

--- ext-home/src/main/java/com/ext/home/App.java
+++ ext-home/src/main/java/com/ext/home/App.java
@@ -6,6 +6,11 @@ package com.ext.home;
  */
 public class App 
 {
+	
+	public static void sayHello(){
+		System.out.println("sayHello()");
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
